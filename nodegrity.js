0afe6a6ad61f4d12fa4272989e6febd2b4b2714d
Julien Biezemans <jb@jbpros.com>
2011-01-08 22:38:29 +0100

    Improve start script error handling

--- nodegrity.js
+++ nodegrity.js
@@ -1,7 +1,8 @@
 var NodegrityServer = require('./lib/nodegrity_server.js');
 
-process.on('uncaughtException', function (err) {
-  console.log('Caught exception: ' + err);
-});
-
-new NodegrityServer({});
+try {
+  new NodegrityServer({port:3333});
+}
+catch (exception) {
+  console.log(exception.stack);
+}
