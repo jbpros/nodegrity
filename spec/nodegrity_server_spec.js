5ef6272a1a1865a6674e541c4d3ee5f3c546ec9a
Julien Biezemans <jb@jbpros.com>
2011-01-08 13:38:13 +0100

    Add HTTP static assets server

--- spec/nodegrity_server_spec.js
+++ spec/nodegrity_server_spec.js
@@ -1,4 +1,6 @@
+var zombie          = require('zombie');
 var NodegrityServer = require('../lib/nodegrity_server');
+
 describe('NodegrityServer', function() {
   var port = 4444;
 
@@ -18,5 +20,33 @@ describe('NodegrityServer', function() {
         var server = NodegrityServer();
       }).toThrow("Missing configuration");
     });
+  });
+
+  it('serves static files through HTTP', function() {
+    var server = NodegrityServer(this.valid_config);
+    var requestFinished = false;
+    zombie.visit("http://localhost:"+this.valid_config['port']+"/", {debug: false}, function(err, browser) {
+      requestFinished = true;
+      expect(err).toBeNull();
+      if (err === null) {
+        expect(browser.lastResponse.status).toBe(200);
+      }
+    });
+    waitsFor(function() { return requestFinished; }, "Time out", 2000);
+  });
+
+  it('servers the main page of the application at /', function() {
+    var server = NodegrityServer(this.valid_config);
+    var requestFinished = false;
+    zombie.visit("http://localhost:"+this.valid_config['port']+"/", {debug: false}, function(err, browser) {
+      requestFinished = true;
+      expect(err).toBeNull();
+      if (err === null) {
+        expect(browser.response[2]).toMatch(/Nodegrity/);
+        expect(browser.response[2]).toMatch(/html/);
+        console.log(browser.response);
+      }
+    });
+    waitsFor(function() { return requestFinished; }, "Time out", 2000);
   });
 });
